50eb37a370554ea22b2a8e7552273f5b9edbc541
jeikerxiao <jeiker@126.com>
2018-02-07 11:38:12 +0800

    add Podfile
    add Podfile to Project

--- RxTodo/RxTodo/AppDelegate.swift
+++ RxTodo/RxTodo/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = .white
+        let viewController = ListViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
---
+++ RxTodo/RxTodo/ViewController/ListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ListViewController.swift
+//  RxTodo
+//
+//  Created by xiao on 2018/2/7.
+//  Copyright © 2018年 jeikerxiao. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "任务列表"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
